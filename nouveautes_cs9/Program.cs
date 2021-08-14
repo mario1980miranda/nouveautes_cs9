3cb25670c031adda65530cd4d43bc36dd78d9029
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-08-14 15:41:44 -0400

    Type Record

--- nouveautes_cs9/Program.cs
+++ nouveautes_cs9/Program.cs
@@ -2,6 +2,16 @@
 
 namespace nouveautes_cs9
 {
+    record PersonneRecord
+    {
+        public string nom { get; set; }
+        public int age { get; set; }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Nom : " + nom + " " + age + " ans.");
+        }
+    }
     struct PersonneStruct
     {
         public string nom { get; set; }
@@ -21,19 +31,35 @@ namespace nouveautes_cs9
         {
             Console.WriteLine("Nom : " + nom + " " + age + " ans.");
         }
+
+        public override bool Equals(object obj)
+        {
+            PersonneClass objetAComparer = (PersonneClass)obj;
+
+            if (nom == objetAComparer.nom && age == objetAComparer.age) return true;
+            return false;
+            //return base.Equals(obj);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var personne1 = new PersonneStruct() { nom = "Mario", age = 40 };
-            var personne2 = personne1;
-
-            personne1.nom = "Cristina";
+            var personne1 = new PersonneRecord() { nom = "Mario", age = 40 };
+            var personne2 = personne1 with { nom = "Cristina" };
 
             //personne1.nom = "Maria";
             personne1.Afficher();
             personne2.Afficher();
+
+            Console.WriteLine(personne1.Equals(personne2));
+
+            /*
+             * Types simples (int, float, char...) -> Value Type (valuer)
+             * structures -> Value Type (valeur = les valeurs des champs)
+             * class -> Reference Type (valeur = adresse de l'objet)
+             *
+             */
 
         }
     }
