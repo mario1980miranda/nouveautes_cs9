1dc5d539b631d116be079b24322827cb25bb3193
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-08-14 15:21:53 -0400

    Les records -> reference de l'objet

--- nouveautes_cs9/Program.cs
+++ nouveautes_cs9/Program.cs
@@ -2,9 +2,19 @@
 
 namespace nouveautes_cs9
 {
-    class Personne
+    struct PersonneStruct
     {
-        public string nom { get; init; }
+        public string nom { get; set; }
+        public int age { get; set; }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Nom : " + nom + " " + age + " ans.");
+        }
+    }
+    class PersonneClass
+    {
+        public string nom { get; set; }
         public int age { get; set; }
 
         public void Afficher()
@@ -16,22 +26,15 @@ namespace nouveautes_cs9
     {
         static void Main(string[] args)
         {
-            var personne1 = new Personne() { nom = "Mario", age = 40 };
+            var personne1 = new PersonneStruct() { nom = "Mario", age = 40 };
+            var personne2 = personne1;
+
+            personne1.nom = "Cristina";
 
             //personne1.nom = "Maria";
             personne1.Afficher();
+            personne2.Afficher();
 
-            Console.WriteLine(
-                    "Le nom de la personne est : " +
-                    personne1.nom
-                );
-
-            /*
-            foreach (var arg in args)
-            {
-                Console.WriteLine(arg);
-            }
-            */
         }
     }
 }
